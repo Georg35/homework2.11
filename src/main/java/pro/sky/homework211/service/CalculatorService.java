51591b3ef7bdb0c65995ca487c5e345d55708d8d
Георгий <georgijstenin@gmail.com>
2023-11-27 16:54:48 +0500

    Дз 2.11 завершена

---
+++ src/main/java/pro/sky/homework211/service/CalculatorService.java
@@ -0,0 +1,9 @@
+package pro.sky.homework211.service;
+
+public interface CalculatorService {
+    String showWelcome();
+    String summa(int usnum1, int usnum2);
+    String minus( int usnum1, int usnum2);
+    String multiply( int usnum1, int usnum2);
+    String divide( int usnum1, int usnum2);
+}
